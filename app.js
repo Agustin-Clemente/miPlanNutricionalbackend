c49a6b77ab064b34aa4ac235bbc89511c4bea0d1
Agush <a_clemente@live.com.ar>
2025-02-02 02:46:21 -0300

    cambios para deploy

--- app.js
+++ app.js
@@ -5,18 +5,11 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/comidas', comidasRouter);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
-export default async function handler(req, res) {
-    app(req, res);
-  };
+export default app;
 
