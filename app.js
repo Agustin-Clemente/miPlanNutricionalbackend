53541058d8cfd001a265fa0ad7baefa9dde0452d
Agush <a_clemente@live.com.ar>
2025-02-02 02:06:47 -0300

    cambios para render

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from 'express';
  import cors from 'cors';
  import comidasRouter from './routes/comidas.js';
+ import 'dotenv/config';
 
 const app = express();
 
-
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/comidas', comidasRouter);
 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+export default async function handler(req, res) {
+    app(req, res);
+  };
 
