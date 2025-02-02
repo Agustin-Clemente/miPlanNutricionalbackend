244b9198be35ccb5256ac3ed8168f4e00bd0767f
Agush <a_clemente@live.com.ar>
2025-02-02 02:14:48 -0300

    sin mongoose

--- routes/comidas.js
+++ routes/comidas.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/* import express from 'express';
 const router = express.Router();
 import Comida from '../models/comida.js';
 
@@ -22,5 +22,69 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: error.message });
     }
  });
+
+export default router; */
+
+import express from 'express';
+const router = express.Router();
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import 'dotenv/config';
+
+// Variables de entorno
+const MONGODB_URI = process.env.MONGODB_URI;
+const client = new MongoClient(MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run(callback) {
+  try {
+    await client.connect();
+    const db = client.db('miPlanNutricional');
+    const comidas = db.collection('comidas');
+    
+      return await callback(comidas);
+    
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+       await client.close()
+    }
+}
+
+// POST /api/comidas
+router.post('/', async (req, res) => {
+  try {
+   const newComida = await run(async (comidas) => {
+     const nuevaComida = {
+        fecha: new Date(req.body.fecha),
+         hora: req.body.hora,
+         comida: req.body.comida,
+         evento: req.body.evento
+     }
+    const result = await comidas.insertOne(nuevaComida)
+      return { ...nuevaComida, _id: result.insertedId};
+  })
+   res.status(201).json(newComida);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// GET /api/comidas
+router.get('/', async (req, res) => {
+   try {
+    const getComidas = await run(async (comidas) => {
+     return await comidas.find().toArray();
+  });
+      res.json(getComidas);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
 export default router;
